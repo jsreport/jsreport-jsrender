ffa56f8d6f9a6bd9284550daa401afc281ea5e65
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-03 17:48:59 +0200

    add support for custom tags

--- test/jsrenderTest.js
+++ test/jsrenderTest.js
@@ -1,7 +1,7 @@
 var should = require("should");
 var jsrender = require("../lib/jsrenderEngine");
 
-describe('jsrender', function(){
+describe('jsrender', function () {
 
   it('should render html', function () {
     var html = jsrender("Hey", null, null);
@@ -9,7 +9,7 @@ describe('jsrender', function(){
   });
 
   it('should be able to use helpers', function () {
-    var html = jsrender("{{>~a()}}", { a: function() { return "Hey"; } }, null);
+    var html = jsrender("{{>~a()}}", { a: function () { return "Hey"; } }, null);
     html.should.be.eql("Hey");
   });
 
@@ -19,19 +19,19 @@ describe('jsrender', function(){
   });
 
   it('should throw when missing helper', function () {
-    should(function() {
+    should(function () {
       jsrender("{{:~missing()}}", null, {});
     }).throw();
   });
 
   it('should throw when syntax error', function () {
-    should(function() {
+    should(function () {
       jsrender("{{:~missing()}}", null, {});
     }).throw();
   });
 
   it('should throw when use constr expression', function () {
-    should(function() {
+    should(function () {
       jsrender('{{:#tmpl.constructor("var foo=3;")()}}', null, {});
     }).throw();
   });
@@ -39,14 +39,45 @@ describe('jsrender', function(){
   it('should be able to parse and use sub tempates', function () {
     var childTemplate = "<script id=\"inner\" type=\"text/x-jsrender\">{{:#data}}</script>";
     var template = "{{for items tmpl=\"inner\"}}{{/for}}";
-    var html = jsrender(childTemplate + template, null, { items : [1,2,3]});
+    var html = jsrender(childTemplate + template, null, { items: [1, 2, 3] });
     html.should.be.eql("123");
   });
 
   it('should be able to parse and use multiple sub tempates', function () {
     var childTemplate = "<script id=\"inner\" type=\"text/x-jsrender\">{{:#data}}</script>\n<script id=\"inner2\" type=\"text/x-jsrender\">a{{:#data}}</script>";
     var template = "{{for items tmpl=\"inner\"}}{{/for}}{{for items tmpl=\"inner2\"}}{{/for}}";
-    var html = jsrender(childTemplate + template, null, { items : [1,2,3]});
+    var html = jsrender(childTemplate + template, null, { items: [1, 2, 3] });
     html.should.be.eql("\n123a1a2a3");
+  });
+
+  it('should be able to use custom tag', function () {
+    var html = jsrender("{{customTag}}{{:a}}{{/customTag}}", {
+      customTag: function () {
+        return this.tagCtx.render(this.tagCtx.view.data)
+      }
+    }, { a: "Hey" });
+    html.should.be.eql("Hey");
+  });
+
+  it('should be able to use custom tag inside for loop', function () {
+    var html = jsrender("{{for people}}{{customTag}}{{:name}}{{/customTag}}{{/for}}", {
+      customTag: function () {
+        return this.tagCtx.render(this.tagCtx.view.data)
+      }
+    }, { people: [{ name: "Jan" }] });
+    html.should.be.eql("Jan");
+  });
+
+  it('should clean tags after render', function () {
+    jsrender("{{customTag}}{{:a}}{{/customTag}}", {
+      customTag: function () {
+        return this.tagCtx.render(this.tagCtx.view.data) + 'x'
+      }
+    }, { a: "Heyx" });
+
+    var html = jsrender("{{customTag}}{{:a}}{{/customTag}}", {}, { a: "Hey" });
+
+    html.should.not.be.eql("Heyx");
+
   });
 });
